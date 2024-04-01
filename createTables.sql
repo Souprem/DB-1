c8f7fd99e8d7eda649891acd5dcf14f7afd5fb3a
Joel Brook <brook.jl@gmail.com>
2024-04-01 18:40:50 -0400

    create tables

--- createTables.sql
+++ createTables.sql
@@ -4,3 +4,37 @@ CREATE TABLE "Card" (
 	"price"	INTEGER,
 	PRIMARY KEY("cardID")
 )
+
+CREATE TABLE "Deck" (
+	"deckID"	INTEGER,
+	"name"	TEXT,
+	"archetype"	TEXT,
+	PRIMARY KEY("deckID")
+)
+
+CREATE TABLE "Tournament" (
+	"tournamentID"	INTEGER,
+	"name"	TEXT,
+	PRIMARY KEY("tournamentID")
+)
+
+CREATE TABLE "Player" (
+	"playerID"	INTEGER,
+	"name"	TEXT,
+	PRIMARY KEY("playerID")
+)
+
+CREATE TABLE "Rankings Tournament" (
+	"deckID"	INTEGER,
+	"tournamentID"	INTEGER,
+	"archetype"	TEXT,
+	FOREIGN KEY("deckID") REFERENCES "Deck"("deckID"),
+    FOREIGN KEY("tournamentID") REFERENCES "Tournament"("tournamentID")
+)
+
+CREATE TABLE "Cards in Deck" (
+  "cardID" INTEGER,
+  "deckID" INTEGER,
+  FOREIGN KEY("deckID") REFERENCES "Deck"("deckID"),
+  FOREIGN KEY("cardID") REFERENCES "Card"("cardID")
+)
